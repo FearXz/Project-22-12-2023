b0671a80649e6b242dc7d1047d71d33627d4e55b
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-12-22 16:58:57 +0100

    quasi ok anche se è senza map

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 /* fixata animazione carosello buggata per overflow-visible*/
 
-let buttons = document.querySelectorAll(".carousel button");
-
+let buttons = document.querySelectorAll(".carousel a[role=button]");
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     let inner = event.currentTarget.closest(".carousel").querySelector(".carousel-inner");
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col, Carousel, Button } from "react-bootstrap";
 
 const Gallery = (props) => {
+  useEffect(() => {
+    const innerCarousel = document.querySelectorAll(".carousel-inner");
+    if (innerCarousel) {
+      innerCarousel.forEach((carousel) => {
+        carousel.classList.add("overflow-visible");
+      });
+    }
+    let buttons = document.querySelectorAll(".carousel a[role=button]");
+    buttons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        let inner = event.currentTarget.closest(".carousel").querySelector(".carousel-inner");
+        inner.classList.add("overflow-hidden");
+        inner.classList.remove("overflow-visible");
+        setTimeout(function () {
+          inner.classList.add("overflow-visible");
+          inner.classList.remove("overflow-hidden");
+        }, 650);
+      });
+    });
+  }, []);
+
   return (
     <Container id={props.id} className="mb-5">
       <h3 className="text-white">{props.title}</h3>
-      <Carousel id="carouselExample" className="slide" indicators={false}>
+      <Carousel id="carouselExample" className="slide" indicators={false} interval={null}>
         <Carousel.Item>
           <Row className="gx-1">
             {props.movieList && props.movieList.length > 0 && (
