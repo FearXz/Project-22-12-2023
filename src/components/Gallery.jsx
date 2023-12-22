7cc6ab8565e2b83862af026c8cb06b8196cbca01
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-12-22 16:47:23 +0100

    ok piccolo fix alt

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,22 +11,22 @@ const Gallery = (props) => {
             {props.movieList && props.movieList.length > 0 && (
               <>
                 <Col xs={4} md={3} xl={2}>
-                  <img src={props.movieList[0].Poster} className="img-fluid" alt="Media 0" />
+                  <img src={props.movieList[0].Poster} className="img-fluid" alt="Media0" />
                 </Col>
                 <Col xs={4} md={3} xl={2}>
-                  <img src={props.movieList[1].Poster} className="img-fluid" alt="Media 1" />
+                  <img src={props.movieList[1].Poster} className="img-fluid" alt="Media1" />
                 </Col>
                 <Col xs={4} md={3} xl={2}>
-                  <img src={props.movieList[2].Poster} className="img-fluid" alt="Media 2" />
+                  <img src={props.movieList[2].Poster} className="img-fluid" alt="Media2" />
                 </Col>
                 <Col className="d-none d-md-block" md={3} xl={2}>
-                  <img src={props.movieList[3].Poster} className="img-fluid" alt="Media 3" />
+                  <img src={props.movieList[3].Poster} className="img-fluid" alt="Media3" />
                 </Col>
                 <Col className="d-none d-xl-block" xl={2}>
-                  <img src={props.movieList[4].Poster} className="img-fluid" alt="Media 4" />
+                  <img src={props.movieList[4].Poster} className="img-fluid" alt="Media4" />
                 </Col>
                 <Col className="d-none d-xl-block" xl={2}>
-                  <img src={props.movieList[5].Poster} className="img-fluid" alt="Media 5" />
+                  <img src={props.movieList[5].Poster} className="img-fluid" alt="Media5" />
                 </Col>
               </>
             )}
@@ -37,22 +37,22 @@ const Gallery = (props) => {
             {props.movieList && props.movieList.length > 0 && (
               <>
                 <Col xs={4} md={3} xl={2}>
-                  <img src={props.movieList[6].Poster} className="img-fluid" alt="Media 0" />
+                  <img src={props.movieList[6].Poster} className="img-fluid" alt="Media6" />
                 </Col>
                 <Col xs={4} md={3} xl={2}>
-                  <img src={props.movieList[7].Poster} className="img-fluid" alt="Media 1" />
+                  <img src={props.movieList[7].Poster} className="img-fluid" alt="Media7" />
                 </Col>
                 <Col xs={4} md={3} xl={2}>
-                  <img src={props.movieList[8].Poster} className="img-fluid" alt="Media 2" />
+                  <img src={props.movieList[8].Poster} className="img-fluid" alt="Media7" />
                 </Col>
                 <Col className="d-none d-md-block" md={3} xl={2}>
-                  <img src={props.movieList[9].Poster} className="img-fluid" alt="Media 3" />
+                  <img src={props.movieList[9].Poster} className="img-fluid" alt="Media9" />
                 </Col>
                 <Col className="d-none d-xl-block" xl={2}>
-                  <img src={props.movieList[0].Poster} className="img-fluid" alt="Media 4" />
+                  <img src={props.movieList[0].Poster} className="img-fluid" alt="Media0" />
                 </Col>
                 <Col className="d-none d-xl-block" xl={2}>
-                  <img src={props.movieList[1].Poster} className="img-fluid" alt="Media 5" />
+                  <img src={props.movieList[1].Poster} className="img-fluid" alt="Media1" />
                 </Col>
               </>
             )}
